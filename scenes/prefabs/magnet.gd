f199473c9cb5b28bd65c9682bf92093d2a68e941
TheGoatIsBetter <thego@thegodesktop.goatnet.local>
2024-03-01 22:18:58 -0800

    mag 2

--- scenes/prefabs/magnet.gd
+++ scenes/prefabs/magnet.gd
@@ -1,14 +1,11 @@
-extends Node2D
+extends RigidBody2D
 
 @export var FORCE = 400
-@export var mass = 2.0
 @export var magnetic_strength = 100000
-var original_mass = 0
 @onready var magnetic_zone: Area2D = $MagneticZone
 @onready var sticky_zone: Area2D = $StickyZone
 var is_magnet_active = false
 
-var sticky_joints = []
 var sticky_objects = []
 var sticky_colliders = []
 var original_sticky_colliders = []
@@ -16,14 +13,14 @@ var original_sticky_colliders = []
 func _input(event):
 	if event.is_action_pressed("magnet"):
 		is_magnet_active = !is_magnet_active
-
-
+	
+		if not is_magnet_active:
+			unstick_objects()
 
 func _physics_process(delta):
 	
 	var direction = Input.get_axis("move_left", "move_right")
-	if get_parent() is RigidBody2D:
-		get_parent().apply_central_force(Vector2.RIGHT * direction * FORCE)
+	apply_central_force(Vector2.RIGHT * direction * FORCE)
 	
 	print(sticky_objects)
 	
@@ -34,85 +31,80 @@ func _physics_process(delta):
 				
 				var force = pull_dir * magnetic_strength * delta
 				object.apply_force(force)
-			
+
+func _on_body_entered(body):
+	if is_magnet_active and body.is_in_group("object") and not body in get_children():
+		var sticky = body.get_node("StickyZone")
+				
+		var sticky_addition = sticky.duplicate()
+		sticky.get_node("CollisionShape2D").disabled = true
+				
+		sticky_zone.add_child(sticky_addition)
+		sticky_addition.global_position = body.position
+				
+		var body_pos = body.position
+		body.get_parent().remove_child(body)
+		self.add_child(body)
+		body.global_position = body_pos
+		body.get_node("CollisionShape2D").disabled = false
+		body.sleeping = true
+		body.gravity_scale = 0
+		body.is_stickied = true
+
+		original_sticky_colliders.append(sticky)
+		sticky_colliders.append(sticky_addition)
+		sticky_objects.append(body)
+	
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	
+	#if body.is_in_group("object"):
+		#var sticky = body.get_node("StickyZone")
+		#sticky.get_node("CollisionShape2D").disabled = false
+		#original_sticky_colliders.erase(sticky)
+		#
+		#var sticky_collider = body.get_node("CollisionShape2D")
+		#sticky_zone.remove_child(sticky_collider)
+		#sticky_colliders.erase(sticky_collider)
+		##sticky_collider.queue_free()
+#
+		#
+		#body.get_node("CollisionShape2D").disabled = false
+		#body.sleeping = false
+		#body.gravity_scale = 1
+		#body.freeze = false
+		#body.is_stickied = false
+#
+		#var body_pos = body.global_position
+		#remove_child(body)
+		#get_parent().get_parent().add_child(body)
+		#body.global_position = body_pos
+
+
+	pass # Replace with function body.
+
+
+func unstick_objects():
+	for sticky_collider in sticky_colliders:
+		sticky_collider.queue_free()
 		
-		for object in sticky_zone.get_overlapping_bodies():
-			if object not in sticky_objects:
-			
-				
-				var sticky = object.get_node("StickyZone")
-				
-				var sticky_addition = sticky.duplicate()
-				sticky.get_node("CollisionShape2D").disabled = true
-				
-				sticky_zone.add_child(sticky_addition)
-				sticky_addition.global_position = object.position
-				
-				var object_pos = object.position
-				object.get_parent().remove_child(object)
-				self.add_child(object)
-				object.global_position = object_pos
-				object.get_node("CollisionShape2D").disabled = false
-				object.sleeping = true
-				object.gravity_scale = 0
-				object.is_stickied = true
-
-				
-				#var joint = DampedSpringJoint2D.new()
-				#
-				#joint.length = 1.1 * object.global_position.distance_to(self.global_position)
-				#joint.rest_length = object.global_position.distance_to(self.global_position)
-				#joint.stiffness = 64
-				#object.add_child(joint)
-				#
-				#joint.position = Vector2.ZERO
-				#
-				#joint.node_a = object.get_path()
-				#joint.node_b = sticky_addition.get_path()
-				
-				#sticky_joints.append(joint)
-				original_sticky_colliders.append(sticky)
-				sticky_colliders.append(sticky_addition)
-				sticky_objects.append(object)
-			
-	else:
-		#for joint in sticky_joints:
-			#joint.queue_free()
-		for sticky_collider in sticky_colliders:
-			sticky_collider.queue_free()
-			
-		for sticky in original_sticky_colliders:
-			sticky.get_node("CollisionShape2D").disabled = false
-			
+	for sticky in original_sticky_colliders:
+		sticky.get_node("CollisionShape2D").disabled = false
 		
-		for object in sticky_objects:
-			object.get_node("CollisionShape2D").disabled = false
-			object.sleeping = false
-			object.gravity_scale = 1
-			object.freeze = false
-			object.is_stickied = false
-			
-			var object_pos = object.global_position
-			remove_child(object)
-			get_parent().get_parent().add_child(object)
-			object.global_position = object_pos
+	for object in sticky_objects:
+		object.get_node("CollisionShape2D").disabled = false
+		object.sleeping = false
+		object.gravity_scale = 1
+		object.freeze = false
+		object.is_stickied = false
 		
-		#sticky_joints = []
-		sticky_objects = []
-		sticky_colliders = []
-		original_sticky_colliders = []
-
-func update_physics():
-	if get_parent() is RigidBody2D:
-		original_mass = get_parent().mass
-		get_parent().mass = mass
-
-	#for joint in sticky_joints:
-		#joint.node_b = get_parent().get_path()
-		
-
-func reset_physics():
-	if get_parent() is RigidBody2D:
-		get_parent().mass = original_mass
-
-
+		var object_pos = object.global_position
+		remove_child(object)
+		get_parent().get_parent().add_child(object)
+		object.global_position = object_pos
+	
+	sticky_objects = []
+	sticky_colliders = []
+	original_sticky_colliders = []
--- scenes/prefabs/object.gd
+++ scenes/prefabs/object.gd
@@ -6,6 +6,7 @@ func _integrate_forces(state):
 	if is_stickied and get_parent() is RigidBody2D:
 		state.linear_velocity = Vector2.ZERO
 		state.angular_velocity = 0
-
-
 		
+		
+
+
