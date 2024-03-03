1542bf2c0efbb6dc6486779729abb805dafdd692
TheGoatIsBetter <thego@thegodesktop.goatnet.local>
2024-03-03 13:44:33 -0800

    credits working

--- globals/pause_manager.gd
+++ globals/pause_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var pause_screen = $CanvasLayer/PauseScreen
+var last_focus = null
 
 var paused = false:
 	set(value):
@@ -13,18 +14,21 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("pause") and not get_tree().current_scene.is_in_group("menu"):
 		paused = !paused
-		if paused:
-			get_tree().paused = true
-			pause_screen.show()
-		else:
-			get_tree().paused = false
-			pause_screen.hide()
+		
 
 func _update_pause_state(pause_state: bool) -> void:
 	if !pause_screen:
 		return
 	get_tree().paused = pause_state
 	pause_screen.visible = pause_state
-		
+	
+	if(pause_state == true):
+		last_focus = get_viewport().gui_get_focus_owner()
+		$CanvasLayer/PauseScreen/MainMenuButton.grab_focus()
+	else:
+		if(last_focus != null):
+			last_focus.grab_focus()
+	
+	
 func _on_main_menu_button_pressed():
 	SceneManager.change_to_menu()
--- globals/scene_manager.gd
+++ globals/scene_manager.gd
@@ -3,6 +3,7 @@ extends Node
 @export var main_menu_scene: PackedScene
 @export var level_scene: PackedScene
 @export var shop_scene: PackedScene
+@export var credits_scene: PackedScene
 
 func change_to_menu():
 	get_tree().change_scene_to_packed(main_menu_scene)
@@ -12,3 +13,6 @@ func change_to_level():
 
 func change_to_shop():
 	get_tree().change_scene_to_packed(shop_scene)
+	
+func change_to_credits():
+	get_tree().change_scene_to_packed(credits_scene)
