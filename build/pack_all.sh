3fddf29badd75e2b26ba6a83f23eccaecbc041be
Xavier Vargas <xtr3m3nerd@gmail.com>
2024-03-03 22:10:48 -0700

    fix build script

--- build/pack_all.sh
+++ build/pack_all.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+GAMENAME="magna_fisher"
+
 # Clean up old
 rm ./linux/*
 rm ./windows/*
 rm ./html/*
 rm ./*.zip
 
-GAMENAME="magna_fisher"
 
 cd ..
-godot --headless --export-debug "Linux/X11" ./build/linux/$GAMENAME.x86-64
-godot --headless --export-debug "Windows Desktop" ./build/windows/$GAMENAME.exe
+godot --headless --export-debug "Linux/X11" ./build/linux/magna_fisher.x86-64
+godot --headless --export-debug "Windows Desktop" ./build/windows/magna_fisher.exe
 godot --headless --export-debug "Web" ./build/html/index.html
 cd build
 
 
 cd linux
-zip -r ../$GAMENAME_linux.zip ./*
+zip -r ../magna_fisher_linux.zip ./*
 cd ../windows
-zip -r ../$GAMENAME_windows.zip ./*
+zip -r ../magna_fisher_windows.zip ./*
 cd ../html
-zip -r ../$GAMENAME_html.zip ./*
+zip -r ../magna_fisher_html.zip ./*
 cd ..
 
